9d218b5506c3ff33475bfa8d70413e1590b13af2
Maxime Mondini <mh-mondini@hotmail.com>
2022-10-29 00:56:31 -0400

    dom selection and manipulation for text

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
-// let apiQuotes =[];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+let apiQuotes =[];
 
 // display random Quote 
 const newQuote = ()=>{
     // pick a random quote from apiQuote array 
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // console.log(quote);
+    quoteAuthor.textContent = quote.author != null || quote.author ? quote.author : 'unknown';
+
+    // styleing quote fontsize
+    if(quote.text.length >= 50){
+        quoteText.classList.add('long-quote');
+        quoteAuthor.classList.add('long-quote');
+    }else {
+        quoteText.classList.remove('long-quote');
+        quoteAuthor.classList.remove('long-quote');
+
+    }
+    quoteText.textContent = quote?.text;
 }
 
 // Get Quotes from API: https://type.fit/api/quotes
 
-// const getQuotes =  async() => {
-//         console.log('hiiiiii');
-//         const apiUrl = 'https://type.fit/api/quotes';
-//         try {
-//             const response = await fetch(apiUrl);
-//             const data = await response.json()
-//             apiQuotes = data;
-//             // console.log(apiQuotes[4])
-//             newQuote();
-//         } catch (error) {
-//             // catch and display error
-//             console.log(error);
-//         }
-// }
+const getQuotes =  async() => {
+        console.log('hiiiiii');
+        const apiUrl = 'https://type.fit/api/quotes';
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json()
+            apiQuotes = data;
+            // console.log(apiQuotes[4])
+            newQuote();
+        } catch (error) {
+            // catch and display error
+            console.log(error);
+        }
+}
 
 
-// getQuotes();
-newQuote();
+getQuotes();
+// newQuote();
