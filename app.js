c67db36791295fe60e1c6415bb815274597b46e8
Maxime Mondini <mh-mondini@hotmail.com>
2022-10-29 00:24:40 -0400

    create quotes.js and fetch localaly quotes

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-let apiQuotes =[];
+// let apiQuotes =[];
 
 // display random Quote 
 const newQuote = ()=>{
     // pick a random quote from apiQuote array 
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
     console.log(quote);
 }
 
 // Get Quotes from API: https://type.fit/api/quotes
 
-const getQuotes =  async() => {
-        console.log('hiiiiii');
-        const apiUrl = 'https://type.fit/api/quotes';
-        try {
-            const response = await fetch(apiUrl);
-            const data = await response.json()
-            apiQuotes = data;
-            // console.log(apiQuotes[4])
-            newQuote();
-        } catch (error) {
-            // catch and display error
-            console.log(error);
-        }
-}
+// const getQuotes =  async() => {
+//         console.log('hiiiiii');
+//         const apiUrl = 'https://type.fit/api/quotes';
+//         try {
+//             const response = await fetch(apiUrl);
+//             const data = await response.json()
+//             apiQuotes = data;
+//             // console.log(apiQuotes[4])
+//             newQuote();
+//         } catch (error) {
+//             // catch and display error
+//             console.log(error);
+//         }
+// }
 
 
-getQuotes();
+// getQuotes();
+newQuote();
